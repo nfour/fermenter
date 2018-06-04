0572f248b57ea957130ffc560dae3ec7f054de4c
nfour <novus.nfour@gmail.com>
2018-06-04 20:40:39 +1000

    todo

--- src/GherkinTest.ts
+++ src/GherkinTest.ts
@@ -1,9 +1,27 @@
-import { GherkinEngine, IGherkinEngineConfig, IGherkinFluid } from './GherkinEngine';
+import { GherkinEngine, IGherkinEngineConfig } from './GherkinEngine';
+import { IGherkinMethods } from './types';
 
-export function GherkinTest ({ feature }: IGherkinEngineConfig, fn: (t: IGherkinFluid) => void) {
-  const t = GherkinEngine({ feature, stackIndex: 3 });
+export function GherkinTest ({ feature }: IGherkinEngineConfig, methodsSetup: (t: IGherkinMethods) => void) {
+  const { builder, ast, methods } = GherkinEngine({ feature, stackIndex: 3 });
+
+  /**
+   * TODO:
+   * - read the feature file, parse out & match:
+   *   - scenario name -> test(name)
+   *   - feature name -> describe(name)
+   *   - background name -> beforeAll(() => background())
+   *   - tags
+   *     - test(`${name} ${tags.map((tag) => `@${tag}`).join(' ')}`)
+   *     - same for the describe()
+   *
+   * - after `methodsSetup`
+   *   - execute the executeFeature based on the now-populated FeatureBuilder instance
+   *
+   * - ???
+   * - prophet
+   */
 
   return test('test', async () => {
-    await fn(t);
+    await methodsSetup(methods);
   });
 }
