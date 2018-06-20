f294cdbb63c9fcfa0b4b18bc524f6ea5e7cf8da8
nfour <novus.nfour@gmail.com>
2018-06-20 15:50:50 +1000

    coverage in tests

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,16 @@
 module.exports = {
-  reporters: [
-    'jest-tap-reporter',
-  ],
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
   setupTestFrameworkScriptFile: './scripts/jestFix.js',
   testRegex: '\.(test|spec)\\.ts$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: ".coverage",
+  coverageReporters: ['text', 'text-summary'],
+  coverageThreshold: {
+    global: { statements: 90, lines: 99, functions: 90 }
+  },
+  reporters: ['jest-tap-reporter'],
   testPathIgnorePatterns: [
     '/build/',
     '/node_modules/'
