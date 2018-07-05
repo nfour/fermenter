ae2e90ba9c3082fff1ad97fccfef756ba7fcd6d5
nfour <novus.nfour@gmail.com>
2018-07-05 11:24:11 -0700

    some decent tests

--- src/tests/calculator.framework.test.ts
+++ src/tests/calculator.framework.test.ts
@@ -38,50 +38,61 @@ const checkResult = ({ c }: { c: number }, expected: number) => {
   expect(c).toBe(expected);
 };
 
-it('runs a GherkinTest', () => {
+const afterAllFn = jest.fn();
+const beforeAllFn = jest.fn();
+const beforeEachFn = jest.fn();
+const afterEachFn = jest.fn();
 
-  GherkinTest({ feature: './features/calculator.feature' }, ({
-    Scenario, Background, ScenarioOutline,
-    AfterAll, BeforeAll, AfterEach, BeforeEach,
-  }) => {
-    Background('Calculator')
-      .Given('I can calculate', () => {
-        expect(Math).toBeTruthy();
-      });
+GherkinTest({ feature: './features/calculator.feature' }, ({
+  Scenario, Background, ScenarioOutline,
+  AfterAll, BeforeAll, AfterEach, BeforeEach,
+}) => {
+  Background('Calculator')
+    .Given('I can calculate', () => {
+      expect(Math).toBeTruthy();
+    });
 
-    Scenario('A simple addition test')
-      .Given('I have the following numbers:', (state = {}, table: IGherkinTableParam) => {
-        const [{ a, b }] = table.rows.mapByTop();
+  Scenario('A simple addition test')
+    .Given('I have the following numbers:', (state = {}, table: IGherkinTableParam) => {
+      const [{ a, b }] = table.rows.mapByTop();
 
-        return {
-          ...state,
-          a: parseInt(a, 10), b: parseInt(b, 10),
-        };
-      })
-      .When('I add the numbers', addNumbers)
-      .And('I do nothing', (state) => state)
-      .Then('I get', (state, text: string) => {
-        expect(state.c).toBe(parseInt(text, 10));
-      });
+      return {
+        ...state,
+        a: parseInt(a, 10), b: parseInt(b, 10),
+      };
+    })
+    .When('I add the numbers', addNumbers)
+    .And('I do nothing', (state) => state)
+    .Then('I get', (state, text: string) => {
+      expect(state.c).toBe(parseInt(text, 10));
+    });
 
-    Scenario('A simple multiplication test')
-      .Given(/^I have numbers (\d+) and (\d+)$/, getNumbers)
-      .When('I multiply the numbers', multiplyNumbers)
-      .Then('I get {int}', checkResult);
+  Scenario('A simple multiplication test')
+    .Given(/^I have numbers (\d+) and (\d+)$/, getNumbers)
+    .When('I multiply the numbers', multiplyNumbers)
+    .Then('I get {int}', checkResult);
 
-    ScenarioOutline('A simple subtraction test')
-      .Given('I have numbers {int} and {int}', getNumbers)
-      .When('I subtract the numbers', subtractNumbers)
-      .Then('I get {int}', checkResult);
+  ScenarioOutline('A simple subtraction test')
+    .Given('I have numbers {int} and {int}', getNumbers)
+    .When('I subtract the numbers', subtractNumbers)
+    .Then('I get {int}', checkResult);
 
-    const afterAllFn = jest.fn();
-    const beforeAllFn = jest.fn();
-    const beforeEachFn = jest.fn();
-    const afterEachFn = jest.fn();
-
-    AfterAll(afterAllFn);
-    BeforeAll(beforeAllFn);
-    BeforeEach(beforeEachFn);
-    AfterEach(afterEachFn);
-  });
+  AfterAll(afterAllFn);
+  BeforeAll(beforeAllFn);
+  BeforeEach(beforeEachFn);
+  AfterEach(afterEachFn);
 });
+
+it('runs a GherkinTest', () => {
+  const expectedAfterAllCalls = 1;
+
+  while (afterAllFn.mock.calls.length !== expectedAfterAllCalls) {
+    break;
+  }
+
+  expect(afterAllFn).toHaveBeenCalledTimes(expectedAfterAllCalls);
+  expect(beforeAllFn).toHaveBeenCalledTimes(1);
+  expect(beforeEachFn).toHaveBeenCalledTimes(4);
+  expect(afterEachFn).toHaveBeenCalledTimes(4);
+
+}, 10000);
