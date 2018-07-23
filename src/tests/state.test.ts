bf0c2cd841759bc4919cfb8971dc759fff65a860
Sam J <nfour@users.noreply.github.com>
2018-07-23 16:05:51 +1000

    Fix background state (#21)
    * tests to prove background state
    
    * add in background fixes

---
+++ src/tests/state.test.ts
@@ -0,0 +1,22 @@
+import { Feature } from '..';
+
+Feature('./features/state.feature', ({ Scenario, Background }) => {
+  const expectedState = { a: 1 };
+
+  Background()
+    .Given('I have some state', () => expectedState)
+    .And('I pass it to another step', (state) => state);
+
+  Scenario('I can inherit state from background')
+    .Then('the state matches', (state) => {
+      expect(state).toEqual(expectedState);
+    });
+
+  const scenarioExpectedState = { x: 1 };
+
+  Scenario('I can create new state for a scenario')
+    .Given('I have some state', () => scenarioExpectedState)
+    .Then('the state matches', (state) => {
+      expect(state).toEqual(scenarioExpectedState);
+    });
+});
