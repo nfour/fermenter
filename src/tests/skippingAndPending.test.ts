5a13c2039d7cbef774b81e418253c7baf8a95dfc
nfour <novus.nfour@gmail.com>
2018-10-11 15:43:17 +1000

    Fix tests

--- src/tests/skippingAndPending.test.ts
+++ src/tests/skippingAndPending.test.ts
@@ -7,6 +7,5 @@ Feature('./features/skippingAndPending.feature', ({ Scenario }) => {
   Scenario.skip('I can skip a test so it does not execute')
     .Then('it is skipped in the test framework', () => 1);
 
-  Scenario('This test isnt tested and should be considered pending')
-    .Then('it is pending in the test framework', () => 1);
+  Scenario('This test isnt tested and should be considered pending');
 });
