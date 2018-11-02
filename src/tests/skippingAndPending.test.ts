be58691a294b445b935bc88bd0957e704b7cd78d
nfour <novus.nfour@gmail.com>
2018-11-02 11:10:55 +1000

    test improvments

--- src/tests/skippingAndPending.test.ts
+++ src/tests/skippingAndPending.test.ts
@@ -1,11 +1,17 @@
 import { Feature } from '../';
 
 Feature('./features/skippingAndPending.feature', ({ Scenario }) => {
+  const fn = jest.fn();
+
   Scenario('I can pass a regular test')
-    .When('it passes in the test framework', () => 1);
+    .When('it passes in the test framework', fn);
 
   Scenario.skip('I can skip a test so it does not execute')
-    .Then('it is skipped in the test framework', () => 1);
+    .Then('it is skipped in the test framework', fn);
 
   Scenario('This test isnt tested and should be considered pending');
+
+  afterAll(() => {
+    if (fn.mock.calls.length !== 1) { process.exit(1); }
+  });
 });
--- src/tests/skippingWithOnly.test.ts
+++ src/tests/skippingWithOnly.test.ts
@@ -1,12 +1,18 @@
 import { Feature } from '../';
 
 Feature('./features/skippingWithOnly.feature', ({ Scenario }) => {
+  const fn = jest.fn();
+
   Scenario('This shouldnt run')
-    .Then('it does not run', () => 1);
+    .Then('it does not run', fn);
 
   Scenario.only('This is the only test that should run')
-    .Then('it runs', () => 1);
+    .Then('it runs', fn);
 
   Scenario('This also shouldnt run')
-    .Then('it does not run', () => 1);
+    .Then('it does not run', fn);
+
+  afterAll(() => {
+    if (fn.mock.calls.length !== 1) { process.exit(1); }
+  });
 });
