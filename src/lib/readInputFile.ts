22a84aa058366083e15526b283c76d96b27e6595
nfour <novus.nfour@gmail.com>
2018-11-12 15:06:40 +1000

    Fix up webpack related fixes

--- src/lib/readInputFile.ts
+++ src/lib/readInputFile.ts
@@ -1,6 +1,11 @@
+declare const __non_webpack_require__: any;
+declare const __webpack_require__: any;
+
 export function readInputFile ({ filePath, testFilePath }: { filePath: string, testFilePath?: string }) {
-  const { dirname, isAbsolute, resolve } = require('path');
-  const { readFileSync } = require('fs');
+  const dRequire = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
+
+  const { dirname, isAbsolute, resolve } = dRequire('path');
+  const { readFileSync } = dRequire('fs');
 
   if (isAbsolute(filePath)) {
     return readFileSync(filePath, 'utf8');
