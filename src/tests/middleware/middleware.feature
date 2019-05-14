9109767ee9d15cae231c7e31b567091b9cdc680b
nfour <novus.nfour@gmail.com>
2019-05-14 12:09:24 +1000

    tdd

--- src/tests/middleware/middleware.feature
+++ src/tests/middleware/middleware.feature
@@ -1,4 +1,4 @@
 Feature: Middleware COR for steps, globally
   Scenario: I have a scenario
-    Given Something is set
-    Then Check if Something is true
+    Given Something is set by a middleware
+    Then Check that Something is true
--- src/tests/middleware/middlewareHookTest.ts
+++ src/tests/middleware/middlewareHookTest.ts
@@ -2,10 +2,10 @@ import { Feature } from '../../';
 
 Feature('./middleware.feature', ({ Scenario }) => {
   Scenario('I have a scenario')
-    .Given('Something is set', () => {
-      return { something: true };
+    .Given('Something is set by a middleware', (initialSomething) => {
+      return { something: initialSomething };
     })
-    .Then('Check if Something is true', (state) => {
+    .Then('Check that Something is true', (state) => {
       expect(state.something).toBe(true);
     });
 });
